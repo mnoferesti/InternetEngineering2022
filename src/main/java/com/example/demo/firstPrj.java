52ff70427372f6e5be02f021b6374af97f7479c9
REZA <effati78@protonmail.com>
2022-11-30 18:53:34 +0330

    [RENAME] the name of the project was changed from "Demo6Application" to "firstPrj"

--- src/main/java/com/example/demo/firstPrj.java
+++ src/main/java/com/example/demo/firstPrj.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo6Application {
+public class firstPrj {
 
     public static void main(String[] args) {
-        SpringApplication.run(Demo6Application.class, args);
+        SpringApplication.run(firstPrj.class, args);
     }
-
 
 }
--- src/test/java/com/example/demo/firstPrjTests.java
+++ src/test/java/com/example/demo/firstPrjTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Demo6ApplicationTests {
+class firstPrjTests {
 
     @Test
     void contextLoads() {
